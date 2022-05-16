f8b2f011ba139f9e260ebae231cf8a669888459b
coderwally <97439397+coderwally@users.noreply.github.com>
2022-05-16 17:44:07 +0200

    Cleanup

--- pages/about-me.js
+++ pages/about-me.js
@@ -7,12 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.js";
 import FooterCustom from "components/Footer/FooterCustom.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
 import Parallax from "components/Parallax/Parallax.js";
 
 import styles from "styles/jss/nextjs-material-kit/pages/landingPage.js";
